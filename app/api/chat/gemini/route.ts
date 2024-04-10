ddb69dc4586b3316fce8d424ca5d3454648ccda4
0xinhua <wuxinhua.cn@gmail.com>
2024-04-11 01:16:18 +0800

    Fix google gemini api

--- app/api/chat/gemini/route.ts
+++ app/api/chat/gemini/route.ts
@@ -1,6 +1,9 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai'
- 
+import { kv } from '@vercel/kv'
+import { auth } from '@/auth'
+import { nanoid } from '@/lib/utils'
+
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '')
  
 // IMPORTANT! Set the runtime to edge
@@ -18,15 +21,48 @@ const buildGoogleGenAIPrompt = (messages: Message[]) => ({
 })
  
 export async function POST(req: Request) {
+
+  const userId = (await auth())?.user.id
   // Extract the `prompt` from the body of the request
-  const { messages } = await req.json()
+  const json = await req.json()
+
+  const { messages } = json
  
   const geminiStream = await genAI
     .getGenerativeModel({ model: 'gemini-pro' })
     .generateContentStream(buildGoogleGenAIPrompt(messages))
  
   // Convert the response into a friendly text-stream
-  const stream = GoogleGenerativeAIStream(geminiStream)
+  const stream = GoogleGenerativeAIStream(geminiStream, {
+    onCompletion: async (completion: string) => {
+      // This callback is called when the completion is ready
+      // You can use this to save the final completion to your database
+      const title = messages[0].content.substring(0, 100)
+      const id = json.id ?? nanoid()
+      const createdAt = Date.now()
+      const path = `/chat/${id}`
+      const payload = {
+        id,
+        title,
+        userId,
+        createdAt,
+        path,
+        messages: [
+          ...messages,
+          {
+            content: completion,
+            role: 'assistant'
+          }
+        ]
+      }
+
+      await kv.hmset(`chat:${id}`, payload)
+      await kv.zadd(`user:chat:${userId}`, {
+        score: createdAt,
+        member: `chat:${id}`
+      })
+    },
+  })
  
   // Respond with the stream
   return new StreamingTextResponse(stream)
