20e708174c4daf6fd442d5af6a4db886a54a5b8b
0xinhua <wuxinhua.cn@gmail.com>
2024-04-11 00:29:29 +0800

    Fix google auth

--- app/actions.ts
+++ app/actions.ts
@@ -33,6 +33,8 @@ export async function getChats(userId?: string | null) {
 export async function getChat(id: string, userId: string) {
   const chat = await kv.hgetall<Chat>(`chat:${id}`)
 
+  console.log('chat', chat, userId)
+
   if (!chat || (userId && chat.userId !== userId)) {
     return null
   }
