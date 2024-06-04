22a36b4eaff3560a701f985a1fdf19bbc9f1076b
0xinhua <wuxinhua.cn@gmail.com>
2024-06-04 15:31:11 +0800

    Update tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"]
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -67,14 +71,34 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "slide-from-left": {
+          '0%': {
+            transform: "translateX(-100%)"
+          },
+          '100%': {
+            transform: "translateX(0)"
+          }
+        },
+        "slide-to-left": {
+          '0%': {
+            transform: "translateX(0)"
+          },
+          '100%': {
+            transform: "translateX(-100%)"
+          }
+        }
       },
       animation: {
+        "slide-from-left":
+          "slide-from-left 0.3s cubic-bezier(0.82, 0.085, 0.395, 0.895)",
+        "slide-to-left":
+          "slide-to-left 0.25s cubic-bezier(0.82, 0.085, 0.395, 0.895)",
         "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+        "accordion-up": "accordion-up 0.2s ease-out"
+      }
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),  require('@tailwindcss/typography')],
 } satisfies Config
 
 export default config
