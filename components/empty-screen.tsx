cd07ca23a2b6292fd4abd75f9131856b27626fa5
0xinhua <wuxinhua.cn@gmail.com>
2024-06-13 19:46:33 +0800

    Release version 0.1.1

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -64,7 +64,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
       <div className="grid md:grid-cols-10 grid-cols-1">
         <div className="rounded-lg bg-background dark:bg-transparent pb-4 sm:pb-8 md:col-span-8">
           <h1 className="mb-2 text-lg font-semibold flex items-center">
-            👋 Welcome to JoyChat <span className="font-normal ml-2 inline-flex items-center rounded-md border border-gray-100 bg-gray-50 px-2 text-sm text-gray-400 dark:border-gray-700/60 dark:bg-gray-800">v0.1.0</span>
+            👋 Welcome to JoyChat <span className="font-normal ml-2 inline-flex items-center rounded-md border border-gray-100 bg-gray-50 px-2 text-sm text-gray-400 dark:border-gray-700/60 dark:bg-gray-800">v0.1.1</span>
           </h1>
           <p className="mb-2 leading-normal text-muted-foreground dark:text-gray-300">
             A beautiful open source AI chatbot built by {' '}
