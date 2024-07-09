369f0cda397943a267b6e713e2ba0e65d7269701
0xinhua <wuxinhua.cn@gmail.com>
2024-07-09 23:38:34 +0800

    Update screenshot png

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: false,
   images: {
     remotePatterns: [
@@ -18,4 +22,4 @@ module.exports = {
     ]
   },
   transpilePackages: ['validation-schemas'],
-}
+})
