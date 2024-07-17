d5be1a9706d1a67e82fa133903b898d225f1b8b8
0xinhua <wuxinhua.cn@gmail.com>
2024-07-17 13:09:33 +0800

    Update vercel json asset prefix

--- next.config.js
+++ next.config.js
@@ -92,5 +92,5 @@ module.exports = withBundleAnalyzer({
     }
   ],
   transpilePackages: ['validation-schemas'],
-  assetPrefix: isProd ? 'https://joychat.io' : undefined
+  assetPrefix: undefined
 })
