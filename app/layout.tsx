c8bd72a4da1f9328771a1bf5e5f917f6e3966dfd
0xinhua <wuxinhua.cn@gmail.com>
2024-07-24 13:43:38 +0800

    Add keywords in meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export const metadata = {
     template: `%s - AI Chatbot access GPT-4o and LLMs all in one place!`
   },
   description: 'Your free AI assistant, a beautiful and secure chatbot alternative to ChatGPT, offering a joyful experience with AI.',
+  keywords: ['AI', 'ai', 'free gpt4o', 'chatgpt', 'ChatGPT locally', 'nextjs chatbot', 'Chat GPT login free', 'ChatGPT nextjs', 'Chat GPT website', 'free chatgpt', 'ChatGPT alternative', 'AI chatbots', 'openai', 'GPT-4o', 'GPT-4o-mini', 'GPT4', 'ai assistant', 'free ai assistant', 'personal AI'],
   icons: {
     icon: [
       {
