a3bac71eeb43b30217fbaa611671c6f0565b4c50
0xinhua <wuxinhua.cn@gmail.com>
2024-07-30 19:03:04 +0800

    Update vercel json rewrites

--- next.config.js
+++ next.config.js
@@ -80,10 +80,6 @@ module.exports = withBundleAnalyzer({
         "destination": "https://joychat-content.vercel.app/content"
       },
       {
-        "source": "/blog/:slug*",
-        "destination": "https://joychat-content.vercel.app/content/blog/:slug*"
-      },
-      {
         source: "/:path(tos|privacy|cookies|about)",
         destination: "https://joychat-content.vercel.app/content/:path*"
       },
